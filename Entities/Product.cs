4fe5445f5f182bc70176bffe07a23be6f44a412c
Bishal <bishalshah431@gmail.com>
2024-11-28 20:51:07 +0545

    Project Created

---
+++ Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductApp.Entities;
+
+public class Product
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public long CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+}
